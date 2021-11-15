408b6d837c16f3243c427efc1ab1ea051727ef05
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2021-11-14 19:16:44 -0600

    assignment complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+const EyesOnMe = () => {
+  return (
+    <button
+      type="button"
+      onFocus={() => console.log('Good!')}
+      onBlur={() => console.log('Hey! Eyes on me!')}
+    >
+      Eyes on me
+    </button>
+  );
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
-// Code Keypad Component Here
+const Keypad = () => {
+  return (
+    <input
+      type="password"
+      onChange={() => console.log('Entering password...')}
+    />
+  );
+};
+
+export default Keypad;
